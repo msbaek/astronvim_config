97f28355167cc4cee5c03b2d67e8e76451d9a40b
Myeongseok Baek <codetemplate@hanmail.net>
2024-04-25 16:53:42 +0900

    change keymaps.lua

--- core/keymaps.lua
+++ core/keymaps.lua
@@ -1,32 +1,11 @@
-return {
-  {
-    "AstroNvim/astrocore",
-    ---@type AstroCoreOpts
-    opts = {
-      mappings = {
-        -- first key is the mode
-        n = {
-          -- second key is the lefthand side of the map
-          -- mappings seen under group name "Buffer"
-          -- ["<leader>bn"] = { "<cmd>bn<cr>" },
-          -- ["<leader>bp"] = { "<cmd>bp<cr>" },
-          ["<Tab>"] = { "<cmd>bn<cr>" },
-          ["<S-Tab>"] = { "<cmd>bp<cr>" },
-          ["<leader>fs"] = { "<cmd>set ft=sql<cr>" },
-          ["<leader>fd"] = { "<cmd>set ft=markdown<cr>" },
-          ["<leader>fj"] = { "<cmd>set ft=java<cr>" },
-          -- ["<leader>ft"] = { "<cmd>set ft=typescript<cr>" },
-          ["<leader>a"] = { "ggVG", desc = "select all" },
-          ["<leader>\\"] = { "<cmd>split<cr>" },
-          ["<leader>|"] = { "<cmd>vsplit<cr>" },
-          -- quick save
-          -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
-        },
-        t = {
-          -- setting a mapping to false will disable it
-          -- ["<esc>"] = false,
-        },
-      },
-    },
-  },
-}
+local map = vim.keymap.set
+
+map("n", "<Tab>", "<cmd>bn<cr>") -- 다음 탭으로 이동
+map("n", "<S-Tab>", "<cmd>bp<cr>") -- 이전 탭으로 이동
+map("n", "<leader>fs", "<cmd>set ft=sql<cr>")
+map("n", "<leader>fd", "<cmd>set ft=markdown<cr>")
+map("n", "<leader>fj", "<cmd>set ft=java<cr>")
+map("n", "<leader>a", "ggVG")
+map("n", "<leader>\\", "<cmd>split<cr>")
+map("n", "<leader>|", "<cmd>vsplit<cr>")
+map("n", "<C-s>", "<cmd>w!<cr>")
